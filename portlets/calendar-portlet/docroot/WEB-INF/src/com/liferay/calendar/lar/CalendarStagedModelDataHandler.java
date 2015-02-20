002850ae8f0de1b83dd6faf5d2cfc0d7d3d0144d
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2015-02-20 18:40:29 -0300

    Revert "LPS-46591 added timeZoneId parameters fix"
    This reverts commit 1759f2efd8fdd2120c372282733547da3fc22cbc.

--- portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/lar/CalendarStagedModelDataHandler.java
+++ portlets/calendar-portlet/docroot/WEB-INF/src/com/liferay/calendar/lar/CalendarStagedModelDataHandler.java
@@ -142,28 +142,25 @@ public class CalendarStagedModelDataHandler
 				importedCalendar = CalendarLocalServiceUtil.addCalendar(
 					userId, portletDataContext.getScopeGroupId(),
 					calendarResourceId, calendarNameMap,
-					calendar.getDescriptionMap(), calendar.getTimeZoneId(),
-					calendar.getColor(), calendar.isDefaultCalendar(),
-					calendar.isEnableComments(), calendar.isEnableRatings(),
-					serviceContext);
+					calendar.getDescriptionMap(), calendar.getColor(),
+					calendar.isDefaultCalendar(), calendar.isEnableComments(),
+					calendar.isEnableRatings(), serviceContext);
 			}
 			else {
 				importedCalendar = CalendarLocalServiceUtil.updateCalendar(
 					existingCalendar.getCalendarId(), calendar.getNameMap(),
-					calendar.getDescriptionMap(), calendar.getTimeZoneId(),
-					calendar.getColor(), calendar.isDefaultCalendar(),
-					calendar.isEnableComments(), calendar.isEnableRatings(),
-					serviceContext);
+					calendar.getDescriptionMap(), calendar.getColor(),
+					calendar.isDefaultCalendar(), calendar.isEnableComments(),
+					calendar.isEnableRatings(), serviceContext);
 			}
 		}
 		else {
 			importedCalendar = CalendarLocalServiceUtil.addCalendar(
 				userId, portletDataContext.getScopeGroupId(),
 				calendarResourceId, calendarNameMap,
-				calendar.getDescriptionMap(), calendar.getTimeZoneId(),
-				calendar.getColor(), calendar.isDefaultCalendar(),
-				calendar.isEnableComments(), calendar.isEnableRatings(),
-				serviceContext);
+				calendar.getDescriptionMap(), calendar.getColor(),
+				calendar.isDefaultCalendar(), calendar.isEnableComments(),
+				calendar.isEnableRatings(), serviceContext);
 		}
 
 		portletDataContext.importClassedModel(calendar, importedCalendar);
